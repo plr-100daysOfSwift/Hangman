c3c1d1bf9e2b2545bc6b88c4639fdcd6109eb4a1
Paul Richardson <developer@photographerafoot.com>
2021-04-25 08:29:44 +1000

    CHG: Build interface in loadView()
    a text field for the word
    a button for each letter of the alphabet

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -10,6 +10,55 @@ import UIKit
 class ViewController: UIViewController {
 
 	var hangman: Hangman?
+	let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "", "Y", "Z", ""]
+	var wordView = UITextField()
+
+	override func loadView() {
+		view = UIView()
+		view.backgroundColor = .white
+
+		wordView.translatesAutoresizingMaskIntoConstraints = false
+		wordView.textAlignment = .center
+		wordView.font = UIFont.systemFont(ofSize: 48)
+		wordView.text = "?????"
+		view.addSubview(wordView)
+
+		let inputView = UIView()
+		inputView.translatesAutoresizingMaskIntoConstraints = false
+		view.addSubview(inputView)
+
+		let rows = 7
+		let columns = 4
+		let width = 44
+		let height = 44
+
+		NSLayoutConstraint.activate([
+
+			wordView.widthAnchor.constraint(equalToConstant: 200),
+			wordView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			wordView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 100),
+
+			inputView.widthAnchor.constraint(equalToConstant: CGFloat(columns * width)),
+			inputView.heightAnchor.constraint(equalToConstant: CGFloat(rows * height)),
+			inputView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -20),
+			inputView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+
+		])
+
+		for row in 0 ..< rows {
+			for column in 0 ..< columns {
+				let letter = alphabet[(columns * row) + column]
+				let button = UIButton(type: .system)
+				button.titleLabel?.font = UIFont.systemFont(ofSize: 24)
+				button.setTitle(letter, for: .normal)
+				button.layer.borderWidth = 0.5
+				let frame = CGRect(x: column * width, y: row * height, width: width, height: height)
+				button.frame = frame
+				inputView.addSubview(button)
+			}
+		}
+
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
