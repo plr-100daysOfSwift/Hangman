82607730357340544bddcaf86e97d3fa92519318
Paul Richardson <developer@photographerafoot.com>
2021-04-24 11:48:47 +1000

    CHG: test incorrect letter is not appended to correctGuesses
    if the guess is incorrect, the collection of correct letters remains unchanged

--- HangmanTests/HangmanTests.swift
+++ HangmanTests/HangmanTests.swift
@@ -63,4 +63,18 @@ class HangmanTests: XCTestCase {
 		XCTAssert(newCount == oldCount + 1, "Correct Guesses has not grown in size")
 	}
 
+	func testChosenLetter_IsInNotWord_CorrectGuessesRemainsConstant() {
+		// Arrange
+		let letter: Character = incorrectLetter
+		let oldCount = sut.correctGuesses.count
+
+		// Act
+		sut.makeGuess(letter: letter)
+		let newCount = sut.correctGuesses.count
+
+		// Assert
+		XCTAssertFalse(sut.correctGuesses.contains(letter), "Correct Guesses should not contain \(letter), but does so: \(sut.correctGuesses.description)")
+		XCTAssert(oldCount == newCount, "Correct guesses has increased by \(newCount - oldCount)")
+	}
+
 }
